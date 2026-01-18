35cf2409ec2775061a53bd460f16eaad403ca765
Alex Pérez <alex21dc@gmail.com>
2026-01-18 14:35:45 +0100

    Prueba de commit desde Eclipse

--- src/main/java/com/miempresa/microservice_demo/controller/TestController.java
+++ src/main/java/com/miempresa/microservice_demo/controller/TestController.java
@@ -8,6 +8,6 @@ public class TestController {
 
     @GetMapping("/test")
     public String test() {
-        return "¡Hola mundo! ¿Como estas?";
+        return "¡Hola mundo! ¿Como estas? Saludos desde Eclipse";
     }
 }
